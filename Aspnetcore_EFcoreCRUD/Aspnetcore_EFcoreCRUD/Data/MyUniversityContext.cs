0e49355a69b7607046b9459edd0f7de924161743
Sreekanth Josyula <sreekanthjo@gmail.com>
2018-06-14 21:39:32 +0530

    MVC core with EF core CRUD Operations

---
+++ Aspnetcore_EFcoreCRUD/Aspnetcore_EFcoreCRUD/Data/MyUniversityContext.cs
@@ -0,0 +1,20 @@
+﻿using MyUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyUniversity.Data
+{
+    public class MyUniversityContext: DbContext
+    {
+        public MyUniversityContext(DbContextOptions<MyUniversityContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
+}
